88d3ee01655aa3bbbcd7bb60ce35cb8ff32d7d6b
Ahmed Fouad <18531447+FoushWare@users.noreply.github.com>
2023-11-03 21:33:21 +0200

    Create soluation01.js

---
+++ Arrays/1920#Build Array from Permutation/soluation01.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function(nums) {
+    let newNum=[]
+    for(let i=0;i<nums.length;i++){
+       newNum[i]=nums[nums[i]];
+    }
+    return newNum;
+    
+};
