cb683f9301cce3f692b44aca2e7f01ba2ba1a0b4
Ahmed Fouad <18531447+FoushWare@users.noreply.github.com>
2023-11-03 21:36:32 +0200

    Create soluation03.js

---
+++ Arrays/1920#Build Array from Permutation/soluation03.js
@@ -0,0 +1,22 @@
+// low space but hight time 
+// This solution utilizes the concept of encoding two values in a single element of the array, 
+// then decoding them to obtain the final result. 
+// It reduces the space complexity from O(n) to O(1) by modifying the input array in place.
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function(nums) {
+  const n = nums.length;
+
+  for (let i = 0; i < n; i++) {
+    nums[i] = nums[i] + n * (nums[nums[i] % n] % n);
+  }
+
+  for (let i = 0; i < n; i++) {
+    nums[i] = Math.floor(nums[i] / n);
+  }
+
+  return nums;
+    
+};
